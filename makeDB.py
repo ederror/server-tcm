06bf4b467c813dcdf08f05822c29dfd029f48441
tlaalsqh7414 <shimmb5454@u.sogang.ac.kr>
2021-08-25 16:35:04 +0900

    create database from trash data

---
+++ makeDB.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+buf = []
+with open("_static/trash_data.csv", "r", encoding="UTF8") as f:
+    lines = f.readlines()
+    for line in lines[1:]:
+        line = line.strip()
+        token = line.split(",")
+        try:
+            buf.append((token[1], token[0], token[2], token[3], token[4]))
+        except:
+            continue
+
+con = sqlite3.connect('_static/trash.db')
+cur = con.cursor()
+
+try:
+    con.execute('CREATE TABLE trash (tid INTEGER, trash_name TEXT, trash_type TEXT, trash_howto_desc TEXT, trash_howto_id INTEGER)')
+except Exception as e:
+    print(e) # table already exists
+
+cur.executemany('INSERT INTO trash VALUES (?, ?, ?, ?, ?)', buf)
+
+con.commit()
+con.close()
